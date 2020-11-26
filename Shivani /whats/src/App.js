ac7a934ddc27fa9aac70d6cc93aee07559b94085
shivanirajpara <71357060+shivanirajpara@users.noreply.github.com>
2020-11-26 00:51:24 -0500

    Add files via upload

---
+++ Shivani /whats/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Pusher from "pusher-js";
+
+function App() {
+  useEffect(() => {
+    const pusher = new Pusher('065254538e85ca4e8f08', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
